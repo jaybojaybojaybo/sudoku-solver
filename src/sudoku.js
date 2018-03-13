635649acd7abd0c6cfe9739a59bdfe8e0d456f92
jasun feddema and johnny mayer <jasun@>
2018-03-13 16:47:27 -0700

    Add base tests, files, proceed with table array function

--- src/sudoku.js
+++ src/sudoku.js
@@ -1,5 +1,22 @@
-export function reorderArray(array) {
-  var inputArray = array;
-  var outputArray = inputArray.sort();
-  return outputArray;
+function sudokify(myTableArray) {
+  let answer = [
+    1,1,1,1,1,1,1,1,1,
+    2,2,2,2,2,2,2,2,2,
+    3,3,3,3,3,3,3,3,3,
+    4,4,4,4,4,4,4,4,4,
+    5,5,5,5,5,5,5,5,5,
+    6,6,6,6,6,6,6,6,6,
+    7,7,7,7,7,7,7,7,7,
+    8,8,8,8,8,8,8,8,8,
+    9,9,9,9,9,9,9,9,9];
+  let output = myTableArray.sort();
+  let wrong = "Sudoku is incorrect.";
+  for (let i = 1; i < 28; i++)
+    if (output != answer)
+    {
+      return wrong;
+    }
+  return output;
 }
+
+exports.sudokifyModule = sudokify;
