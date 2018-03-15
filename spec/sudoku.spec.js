17df70ec173cc2cf2c1daf3aedebfe6170788dc9
Jasun Feddema and Johnny Mayer <jasun+johnny@>
2018-03-15 16:55:03 -0700

    Add test, proceed with testing environments

--- spec/sudoku.spec.js
+++ spec/sudoku.spec.js
@@ -1,7 +1,7 @@
-import {SudokuBoard} from '../src/sudoku';
+import {SudokuBoard} from '../src/sudoku'
 
-describe('SudokuBoard', function(){
-  it('should be an array', function(){
+describe('SudokuBoard', function () {
+  it('should be an array', function () {
     let testArray = new SudokuBoard([
       [],
       [],
@@ -12,11 +12,11 @@ describe('SudokuBoard', function(){
       [],
       [],
       []
-    ]);
-    expect(testArray.sudokuArray).toBeArray(true);
+    ])
+    expect(testArray.sudokuArray).toBeArray(true)
   })
 
-  it('should be an array containing 9 individual arrays', function(){
+  it('should be an array containing 9 individual arrays', function () {
     let testArray = new SudokuBoard([
       [],
       [],
@@ -27,76 +27,96 @@ describe('SudokuBoard', function(){
       [],
       [],
       []
-    ]);
-    expect(testArray.sudokuArray).toBeNonEmptyArray(true);
+    ])
+    expect(testArray.sudokuArray).toBeNonEmptyArray(true)
   })
 
-  it('should compare array at index [0] to an answer', function(){
-    const answer = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  it('should compare array at index [0] to an answer', function () {
+    const answer = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     let testArray = new SudokuBoard([
-          [8,3,7,2,5,4,9,1,6],
-          [9,2,1,8,7,6,5,3,4],
-          [5,4,6,3,9,1,8,2,7],
-          [6,5,2,1,4,8,7,9,3],
-          [1,8,9,7,6,3,2,4,5],
-          [3,7,4,9,2,5,1,6,8],
-          [7,6,5,4,1,2,3,8,9],
-          [2,9,8,6,3,7,4,5,1],
-          [4,1,3,5,8,9,6,7,2]]
-        );
-    let sortedArray = testArray.sudokuArray[0].sort();
-      expect(sortedArray).toEqual(answer);
+      [8, 3, 7, 2, 5, 4, 9, 1, 6],
+      [9, 2, 1, 8, 7, 6, 5, 3, 4],
+      [5, 4, 6, 3, 9, 1, 8, 2, 7],
+      [6, 5, 2, 1, 4, 8, 7, 9, 3],
+      [1, 8, 9, 7, 6, 3, 2, 4, 5],
+      [3, 7, 4, 9, 2, 5, 1, 6, 8],
+      [7, 6, 5, 4, 1, 2, 3, 8, 9],
+      [2, 9, 8, 6, 3, 7, 4, 5, 1],
+      [4, 1, 3, 5, 8, 9, 6, 7, 2]]
+    )
+    let sortedArray = testArray.sudokuArray[0].sort()
+    expect(sortedArray).toEqual(answer)
   })
 
-  it('should compare each array in a parent array to the answer', function(){
-    const answer = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    let testArray = new SudokuBoard([
-          [8,3,7,2,5,4,9,1,6],
-          [9,2,1,8,7,6,5,3,4],
-          [5,4,6,3,9,1,8,2,7],
-          [6,5,2,1,4,8,7,9,3],
-          [1,8,9,7,6,3,2,4,5],
-          [3,7,4,9,2,5,1,6,8],
-          [7,6,5,4,1,2,3,8,9],
-          [2,9,8,6,3,7,4,5,1],
-          [4,1,3,5,8,9,6,7,2]]
-        );
-    let submittedArray = testArray.sudokuCheck();
-    expect(submittedArray.length).toEqual(testArray.sudokuArray.length);
-  })
+  // it('should compare each array in a parent array to the answer', function(){
+  //   const answer = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  //   let testArray = new SudokuBoard([
+  //         [8,3,7,2,5,4,9,1,6],
+  //         [9,2,1,8,7,6,5,3,4],
+  //         [5,4,6,3,9,1,8,2,7],
+  //         [6,5,2,1,4,8,7,9,3],
+  //         [1,8,9,7,6,3,2,4,5],
+  //         [3,7,4,9,2,5,1,6,8],
+  //         [7,6,5,4,1,2,3,8,9],
+  //         [2,9,8,6,3,7,4,5,1],
+  //         [4,1,3,5,8,9,6,7,2]]
+  //       );
+  //   let submittedArray = testArray.sudokuCheck();
+  //   expect(submittedArray.length).toEqual(testArray.sudokuArray.length);
+  // })
 
-  it('should return "Hear my cries, hear my call!"', function(){
-    const answer = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    let testArray = new SudokuBoard([
-          ["a"],
-          ["a"],
-          ["a"],
-          ["a"],
-          ["a"],
-          ["a"],
-          ["a"],
-          ["a"],
-          ["a"]]
-        );
-    let submittedArray = testArray.sudokuCheck();
-    expect(submittedArray).toEqual("Hear my cries, hear my call!");
-  })
+  // it('should add element from array at specific index to a new array', function(){
+  //   const answer = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  //   let testArray = new SudokuBoard([
+  //         [8,3,7,2,5,4,9,1,6],
+  //         [9,2,1,8,7,6,5,3,4],
+  //         [5,4,6,3,9,1,8,2,7],
+  //         [6,5,2,1,4,8,7,9,3],
+  //         [1,8,9,7,6,3,2,4,5],
+  //         [3,7,4,9,2,5,1,6,8],
+  //         [7,6,5,4,1,2,3,8,9],
+  //         [2,9,8,6,3,7,4,5,1],
+  //         [4,1,3,5,8,9,6,7,2]]
+  //       );
+  //   let testVerts = [8,9,5,6,1,3,7,2,4];
+  //   let resultVerts = testArray.vertify();
+  //   expect(testVerts).toEqual(resultVerts[0]);
+  // })
 
-  it('should add element from array at specific index to a new array', function(){
-    const answer = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  // it('should cut sudokuArray into 9 grid arrays', function(){
+  //   const answer = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  //   let testArray = new SudokuBoard([
+  //         [8,3,7,2,5,4,9,1,6],
+  //         [9,2,1,8,7,6,5,3,4],
+  //         [5,4,6,3,9,1,8,2,7],
+  //         [6,5,2,1,4,8,7,9,3],
+  //         [1,8,9,7,6,3,2,4,5],
+  //         [3,7,4,9,2,5,1,6,8],
+  //         [7,6,5,4,1,2,3,8,9],
+  //         [2,9,8,6,3,7,4,5,1],
+  //         [4,1,3,5,8,9,6,7,2]]
+  //       );
+  //   let testGrids = [6,5,2,1,8,9,3,7,4];
+  //   let resultGrids = testArray.gridify();
+  //   console.log(resultGrids);
+  //   expect(testGrids).toEqual(resultGrids[1]);
+  // })
+
+  it('should return a parent array containing 27 arrays', function () {
+    const answer = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     let testArray = new SudokuBoard([
-          [8,3,7,2,5,4,9,1,6],
-          [9,2,1,8,7,6,5,3,4],
-          [5,4,6,3,9,1,8,2,7],
-          [6,5,2,1,4,8,7,9,3],
-          [1,8,9,7,6,3,2,4,5],
-          [3,7,4,9,2,5,1,6,8],
-          [7,6,5,4,1,2,3,8,9],
-          [2,9,8,6,3,7,4,5,1],
-          [4,1,3,5,8,9,6,7,2]]
-        );
-    let testVerts = [8,9,5,6,1,3,7,2,4];
-    let resultVerts = testArray.sudokufy();
-    expect(testVerts).toEqual(resultVerts[0]);
+      [8, 3, 7, 2, 5, 4, 9, 1, 6],
+      [9, 2, 1, 8, 7, 6, 5, 3, 4],
+      [5, 4, 6, 3, 9, 1, 8, 2, 7],
+      [6, 5, 2, 1, 4, 8, 7, 9, 3],
+      [1, 8, 9, 7, 6, 3, 2, 4, 5],
+      [3, 7, 4, 9, 2, 5, 1, 6, 8],
+      [7, 6, 5, 4, 1, 2, 3, 8, 9],
+      [2, 9, 8, 6, 3, 7, 4, 5, 1],
+      [4, 1, 3, 5, 8, 9, 6, 7, 2]]
+    )
+    let checkedArray = testArray.sudokuCheck()
+    console.log(checkedArray)
+    expect(27).toEqual(checkedArray.length)
   })
 })
--- src/sudoku.js
+++ src/sudoku.js
@@ -1,34 +1,61 @@
 export class SudokuBoard {
   constructor (sudokuArray) {
-    this.sudokuArray = sudokuArray;
-    this.answer = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    this.sudokuArray = sudokuArray
+    this.answer = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   }
 
-  sudokuCheck() {
-    let checkedArrays = [];
-    for(let i = 0; i < this.sudokuArray.length; i++){
-      let sortedArray = this.sudokuArray[i].sort();
-      let checkedArray = [];
-      for(let j = 0; j < sortedArray.length; j++){
-        if(sortedArray[j] === this.answer[j]){
-          checkedArray.push(this.sudokuArray[i][j]);
+  vertify () {
+    let verts = [[], [], [], [], [], [], [], [], []]
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < this.sudokuArray[i].length; j++) {
+        verts[i].push(this.sudokuArray[j][i])
+      }
+      console.log(verts[j])
+      this.sudokuArray.push(verts[i])
+    }
+    return verts
+  }
+
+  gridify () {
+    let firstGrids = [[], [], []]
+    for (let i = 0; i < this.sudokuArray.length; i++) {
+      firstGrids[0].push(this.sudokuArray[i].slice(0, 3))
+      firstGrids[1].push(this.sudokuArray[i].slice(3, 6))
+      firstGrids[2].push(this.sudokuArray[i].slice(6))
+    }
+    let secondGrids = [[], [], [], [], [], [], [], [], []]
+    secondGrids[0] = [].concat.apply([], firstGrids[0].slice(0, 3))
+    secondGrids[1] = [].concat.apply([], firstGrids[0].slice(3, 6))
+    secondGrids[2] = [].concat.apply([], firstGrids[0].slice(6))
+    secondGrids[3] = [].concat.apply([], firstGrids[1].slice(0, 3))
+    secondGrids[4] = [].concat.apply([], firstGrids[1].slice(3, 6))
+    secondGrids[5] = [].concat.apply([], firstGrids[1].slice(6))
+    secondGrids[6] = [].concat.apply([], firstGrids[2].slice(0, 3))
+    secondGrids[7] = [].concat.apply([], firstGrids[2].slice(3, 6))
+    secondGrids[8] = [].concat.apply([], firstGrids[2].slice(6))
+    for (let j = 0; j < secondGrids.length; j++) {
+      this.sudokuArray.push(secondGrids[j])
+    }
+    return secondGrids
+  }
+
+  sudokuCheck () {
+    this.vertify()
+    this.gridify()
+    let massiveArray = this.sudokuArray
+    let checkedArrays = []
+    for (let i = 0; i < massiveArray.length; i++) {
+      let sortedArray = massiveArray[i].sort()
+      let checkedArray = []
+      for (let j = 0; j < sortedArray.length; j++) {
+        if (sortedArray[j] === this.answer[j]) {
+          checkedArray.push(this.sudokuArray[i][j])
         } else {
-          return "Hear my cries, hear my call!";
+          return 'Hear my cries, hear my call!'
         }
       }
-      checkedArrays.push(checkedArray);
+      checkedArrays.push(checkedArray)
     }
-    return checkedArrays;
-  }
-
-  sudokufy() {
-    let verts = [[],[],[],[],[],[],[],[],[]];
-    for(let i = 0; i < this.sudokuArray.length; i++){
-      for(let j = 0; j < this.sudokuArray[i].length; j++){
-        verts[i].push(this.sudokuArray[j][i]);
-        console.log(verts);
-      }
-    }
-    return verts;
+    return checkedArrays
   }
 }
